5fa227d980dbc3b07ff4157db99169135a6c061f
Dannel Jurado <dannelj@gmail.com>
2020-09-18 14:13:42 -0400

    Update make_commits.py

--- make_commits.py
+++ make_commits.py
@@ -21,7 +21,21 @@ messages=[
 	"fuck",
 	"wip",
 	"bump version",
-	"updates"	
+	"updates",
+	"ah dammit, forgot this",
+	"if I write it correctly the first time, I should get an award",
+	"when does code exist? WHY does it exist?",
+	"Do you think Zalgo willl forgive mine sins?",
+	"Ain't no party like a empty space party",
+	"HEAD empty, no commmits",
+	"sometimes I wonder if computers fear us, I certainly fear them",
+	"if you press the button, you receive candy",
+	"If everybody's green, nobody's green",
+	"http://gph.is/2jAdoZw",
+	"Someone said gif commit messages were unprofessional, I don't like that Someone",
+	"A scrub is a guy that thinks he's fly and is also known as a buster",
+	"I miss when songs would introduce the concepts they talked about",
+	"Last commit *insert Final Countdown music*"
 ]
 
 # I couldn't get the bash for loop to run in the github action, so... here we are.
